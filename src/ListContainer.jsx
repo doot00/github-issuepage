74561b9caaa96df03e9de49d8d88a0506eec1085
doot00 <binnung_s@naver.com>
2025-01-28 17:13:57 +0900

    페이지네이션, open-closed 상태 toggle
    pagenation, open-closed 상태 toggle

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -15,18 +15,22 @@ export default function ListContainer() {
   const [checked, setChecked] = useState(false);
   const [list, setList] = useState([]);
   const [page, setPage] = useState(1);
+  const [isOpenMode, setIsOpenMode] = useState(true);
   const maxPage = 10;
 
-  async function getData() {
+  async function getData(params) {
     const { data } = await axios.get(
-      `https://api.github.com/repos/facebook/react/issues`
+      `https://api.github.com/repos/facebook/react/issues`,
+      { params }
     );
     setList(data); // data라는 객체 안에 30개 array로 있다.
   }
 
   useEffect(() => {
-    getData();
-  }, []); // componentDidMount(); 돔이 그려진 후에 getData가 실행된다.
+    getData({ page, state: isOpenMode ? "open" : "closed" });
+  }, [page, isOpenMode]); // componentDidMount(); 돔이 그려진 후에 getData가 실행된다. 페이지의 변화를 알아챘기 때문이다.
+
+  console.log({ list });
 
   return (
     <>
@@ -47,7 +51,10 @@ export default function ListContainer() {
             New Issue
           </Button>
         </div>
-        <OpenClosedFilters />
+        <OpenClosedFilters
+          isOpenMode={isOpenMode}
+          onCLickMode={setIsOpenMode}
+        />
         <div className={styles.container}>
           <ListItemLayout className={styles.listFilter}>
             <ListFilter
@@ -68,7 +75,7 @@ export default function ListContainer() {
         </div>
         <div className={styles.paginationContainer}>
           <Pagination
-            maxPage={10}
+            maxPage={maxPage}
             currentPage={page}
             onClickPageButton={(number) => setPage(number)}
           />
@@ -79,24 +86,22 @@ export default function ListContainer() {
   );
 }
 
-function OpenClosedFilters({ data }) {
-  const [isOpenMode, setIsOpenMode] = useState(true);
-  const openModeDataSize = 1;
-  const closeModeDataSize = 2;
+function OpenClosedFilters({ isOpenMode, onCLickMode }) {
+  //const [isOpenMode, setIsOpenMode] = useState(true);
 
   return (
     <>
       <OpenClosedFilter
-        size={openModeDataSize}
+        ///size={openModeDataSize}
         state="Open"
         selected={isOpenMode}
-        onClick={() => setIsOpenMode(true)}
+        onClick={() => onCLickMode(true)}
       />
       <OpenClosedFilter
-        size={closeModeDataSize}
+        //size={closeModeDataSize}
         state="Closed"
         selected={!isOpenMode}
-        onClick={() => setIsOpenMode(false)}
+        onClick={() => onCLickMode(false)}
       />
     </>
   );
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,11 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import React from "react";
 import styles from "./ListItem.module.css";
 import ListItemLayout from "./ListItemLayout";
 import Badge from "./Badge";
+
+dayjs.extend(relativeTime);
 
 export default function ListItem({
   checked,
@@ -10,6 +14,9 @@ export default function ListItem({
   data,
 }) {
   const badges = data.labels;
+  const state = data.state === "open" ? "opened" : "closed";
+  const date = data.state === "open" ? data.created_at : data.closed_at;
+  // 순수 javascript보다 라이브러리를 사용해야 한다.
 
   return (
     <ListItemLayout checked={checked} onClick={onClickCheckBox}>
@@ -17,13 +24,12 @@ export default function ListItem({
         <div role="button" onClick={onClickTitle} className={styles.title}>
           {data.title}
           {badges.length > 0 &&
-            badges.map((badgeProps, idx) => (
-              <Badge key={`${idx}`} {...badgeProps} />
-            ))}
+            badges.map((props, idx) => <Badge {...props} key={`${idx}`} />)}
         </div>
-        <div className={styles.description}># Description</div>
+        <div className={styles.description}>
+          #{data.number} {state} {dayjs(date).fromNow()} by {data.user.login}
+        </div>
       </div>
     </ListItemLayout>
   );
 }
-// hover, checkbox, title과 subtitle이 들어가있다.
